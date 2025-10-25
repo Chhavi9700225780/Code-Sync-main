e9170a9e3cc3831a7298b26500145e15f2c78d04
Chhavi9700225780 <c9081741@gmail.com>
2025-10-25 13:42:28 +0530

    done

---
+++ Terraform/backend.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.13.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.14.1"
+    }
+  }
+
+  cloud {}
+}
+provider "aws" {
+  region = var.aws-region
+}
+
+terraform { 
+  cloud { 
+    
+    organization = "CodeSyncProject" 
+
+    workspaces { 
+      name = "CodeSyncProject" 
+    } 
+  } 
+}
